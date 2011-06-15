2e4c7ad139e5c65eeac827aaec7765ee7c6d95c5
Frank Smieja <frank.smieja@smartatech.com>
2011-06-15 22:22:53 +0100

    more changes

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -2,9 +2,11 @@ require 'spec_helper'
 
 describe EventsController do
 
-  describe "GET 'home'" do
+  describe "GET 'events'" do
     it "should be successful" do
-      get 'home'
+      get 'events'
       response.should be_success
     end
   end
+
+end
